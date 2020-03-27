b8ebf4716c75069d94172277636392bd7779f721
Michael Romay <amerror@gmail.com>
2020-03-27 10:57:42 -0700

    actors get all route and test

--- __tests__/actors.test.js
+++ __tests__/actors.test.js
@@ -1,23 +1,38 @@
-const { getActor, getActors } = require('../lib/helpers/data-helpers');
+const { getActor, getActors } = require('../db/data-helpers');
 
 const request = require('supertest');
 const app = require('../lib/app');
 
 describe('actor routes', () => {
+
   it('creates an actor', () => {
     return request(app)
       .post('/api/v1/actors')
       .send({
-        name: 'A Budge Runs Through It'
+        name: 'Budge Wootie'
       })
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
           __v: 0,
-          name: 'A Budge Runs Through It',
+          name: 'Budge Wootie',
           id: expect.any(String)
         });
       });
   });
+
+  it('gets all actors', async() => {
+    const actors = await getActors();
+    return request(app)
+      .get('/api/v1/actors')
+      .then(res => {
+        actors.forEach(actor => {
+          delete actor.__v;
+          expect(res.body).toContainEqual(actor);
+        });
+      });
+  });
+
+
 
 });
